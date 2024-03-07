5da1051f32b5a61c8c862e39979ba6469bed620c
Anna Powaska <annapowaska@op.pl>
2024-03-07 12:51:58 +0100

    JS repetition

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+var computerMove;
+computerMove = 'kamień';
+
+var playerMove
+
+playerMove = "Papier"
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
+
+var randomNumber;
+randomNumber = Math.floor(Math.random() * 10 + 1);
+printMessage('Wylosowana liczba to: ' + randomNumber);
