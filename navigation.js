023da67e1a031b5b14b45ffed0497c15cf4e84b7
Anna Powaska <annapowaska@op.pl>
2024-03-13 13:50:54 +0100

    .

--- navigation.js
+++ navigation.js
@@ -1,10 +1,16 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault
+  const clickedItem = this;
   console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
+const portfolioLinks = document.querySelectorAll('.nav-list a');
 
+for(let portfolioLink of portfolioLinks){
+    portfolioLink.classList.remove('active')
+}
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
