49e856dd5aee6c1c443293652e143f18280b2c1a
Anna Powaska <annapowaska@op.pl>
2024-03-13 15:09:10 +0100

    Add interactive nav for projects

--- navigation.js
+++ navigation.js
@@ -3,26 +3,30 @@
 function titleClickHandler(event){
   event.preventDefault
   const clickedItem = this;
-  console.log('Link was clicked!');
 
   /* remove class 'active' from all article links  */
-const portfolioLinks = document.querySelectorAll('.nav-list a');
+const portfolioLinks = document.querySelectorAll('.list a.active');
 
 for(let portfolioLink of portfolioLinks){
     portfolioLink.classList.remove('active')
 }
   /* add class 'active' to the clicked link */
-
+  clickedItem.classList.add('active')
   /* remove class 'active' from all articles */
+const activeGalleries = document.querySelectorAll('.row-1.active');
 
+for(let activeGallery of activeGalleries){
+    activeGallery.classList.remove('active')
+}
   /* get 'href' attribute from the clicked link */
-
+  const linkAttribute = clickedItem.getAttribute('href');
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const matchedGallery = document.querySelector(linkAttribute);
   /* add class 'active' to the correct article */
+matchedGallery.classList.add('active')
 }
 
-const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll('.nav-list a');
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
