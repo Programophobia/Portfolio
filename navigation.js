f910e70612e2ea6f6a8c58c55147ee42a4a68cbe
Anna Powaska <annapowaska@op.pl>
2024-03-16 05:34:53 +0100

    Nie działa

--- navigation.js
+++ navigation.js
@@ -3,49 +3,43 @@
 function titleClickHandler(event){
   event.preventDefault
   const clickedItem = this;
+  const portfolioLinks = document.querySelectorAll('.list a.active');
 
-  /* remove class 'active' from all article links  */
-const portfolioLinks = document.querySelectorAll('.list a.active');
-
-for(let portfolioLink of portfolioLinks){
-    portfolioLink.classList.remove('active')
-}
-  /* add class 'active' to the clicked link */
+  for(let portfolioLink of portfolioLinks){
+      portfolioLink.classList.remove('active')
+  }
   clickedItem.classList.add('active')
-  /* remove class 'active' from all articles */
-const activeGalleries = document.querySelectorAll('.row-1.active');
 
-for(let activeGallery of activeGalleries){
-    activeGallery.classList.remove('active')
-}
-  /* get 'href' attribute from the clicked link */
+  const activeGalleries = document.querySelectorAll('.row-1.active');
+
+  for(let activeGallery of activeGalleries){
+      activeGallery.classList.remove('active')
+  }
+
   const linkAttribute = clickedItem.getAttribute('href');
-  /* find the correct article using the selector (value of 'href' attribute) */
   const matchedGallery = document.querySelector(linkAttribute);
-  /* add class 'active' to the correct article */
-matchedGallery.classList.add('active')
+
+  matchedGallery.classList.add('active')
 }
 
 const links = document.querySelectorAll('.nav-list a');
+const btns = document.querySelectorAll('.btn-links a')
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
 
-const btns = document.querySelectorAll('.btn-links a')
-
 for(let btn of btns){
     btn.addEventListener('click', titleClickHandler);
   }
 
-  function printAnna(msg) {
-    const anna = document.createElement('p');
-    anna.innerHTML = msg;
-    document.getElementById('anka').appendChild(anna);
-  }
+function printAnna(msg) {
+  const anna = document.createElement('p');
+  anna.innerHTML = msg;
+  document.getElementById('anka').appendChild(anna);
+}
 
-  printAnna('Anna Powąska');
-
+printAnna('Anna Powąska');
 
 
 
